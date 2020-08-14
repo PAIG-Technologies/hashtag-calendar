772f8305cdc140aac8fc617301a6c207c2c06272
Ramesh Bhatta <ramesh.paig@outlook.com>
2020-08-14 16:39:55 +1000

    calendar integrated

--- functions.php
+++ functions.php
@@ -9,9 +9,12 @@ define("HASHTAG_CHILD_URL",get_stylesheet_directory_uri());
 add_action("wp_enqueue_scripts","add_stylesheet_script");
 
 function add_stylesheet_script(){
+    wp_enqueue_style('tailwind-css', "https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css");
     $css_folder=get_stylesheet_directory_uri()."/assets/css/";
     wp_enqueue_style( 'base-ndis-style', $css_folder."style.css", array(), HASHTAG_STYLE_VERSION );
     wp_enqueue_style( 'ndis-user', $css_folder."user.css", array(), HASHTAG_STYLE_VERSION );
     wp_enqueue_style( 'ndis-color', $css_folder."color.css", array(), HASHTAG_STYLE_VERSION );
     wp_enqueue_style( 'ndis-style', get_stylesheet_uri(), array(), HASHTAG_STYLE_VERSION );
 }
+
+
